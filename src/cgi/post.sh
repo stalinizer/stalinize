fb2a231ceb765fcf8f3c5d23c1c337554b092f2e
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-04-07 10:27:49 -0500

    libcgi is messing with the input

---
+++ src/cgi/post.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: multipart/form-data; boundary=----------------------------4ebf00fbcf09" --data-binary @test.png http://localhost:8080/stalinize.cgi
--- src/cgi/stalinize.c
+++ src/cgi/stalinize.c
@@ -1,63 +1,78 @@
-#include <stdio.h>
+//#include <stdio.h>
 #include <time.h>
 #include <cgi.h>
 
 #include <string>
+#include <cstring>
 #include <cstdlib>
 
-#include <cstdio>
+//#include <cstdio>
 #include <iostream>
-#include <fstream>
+//#include <fstream>
 
-void process_image(std::ofstream & of, int content_length, std::string content_type) {
-int blocksize = 256;
-char * buffer = new char [blocksize];
-std::cin.read (buffer,blocksize);
-if (std::cin) { 
-printf("read %d",std::cin.gcount());
-of.write(buffer, std::cin.gcount());
-}
+void process_image(
+                   int content_length, 
+                   std::string content_type
+                   ) 
+{
+  char buffer[content_length];
+  memset(buffer,' ', content_length);  
+  std::cin.read(buffer, content_length);
+  int readcount = std::cin.gcount();  
+  printf("<p>read size %d\n", readcount);
+  printf("<p>read buffer %d\n<table>", std::strlen(buffer));
+  //  printf("<p>raw:\"%s\"\n",buffer); 
+  for (int i=0; i < content_length; i++){
+    printf("<tr><td>%d</td><td>%X</td></tr>\n",
+           i, 
+           //           buffer[i],
+           buffer[i]);
+  }
+  printf("</table>");
+  // cv::Mat data_mat(data);
+  // cv::Mat frame(imdecode(data_mat,1));
 }
 
 int main()
 {	
-	cgi_init();
-	cgi_process_form();	
-	cgi_init_headers();
+  //	cgi_init();
+  //	cgi_process_form();	
+  //	cgi_init_headers();
+  printf("Content-Type: text/html\n\n");
 
-	cgi_include("stalinize.html");
+  cgi_include("stalinize.html");
 
-	if(std::getenv("CONTENT_TYPE")!=NULL)
-	  printf("content type:%s",getenv("CONTENT_TYPE"));
+  if(std::getenv("CONTENT_TYPE")!=NULL)
+    printf("<p>content type:%s",getenv("CONTENT_TYPE"));
+  
+  if(getenv("QUERY_STRING")!=NULL)
+    printf("<p>DEBUG:%s\n",getenv("QUERY_STRING"));
+  
+  if(getenv("CONTENT_LENGTH")!=NULL)	  
+    printf( "<p>LEN:%s",getenv("CONTENT_LENGTH") );
+  
+  if( getenv("HTTP_ACCEPT_LANGUAGE") )
+    printf( "<p>Lang:%s", getenv("HTTP_ACCEPT_LANGUAGE") );
+  
+  if(getenv("CONTENT_LENGTH")!=NULL){
+    int _CONTENT_LENGTH = atoi( getenv("CONTENT_LENGTH"));
+    printf("<p>content len %d\n", _CONTENT_LENGTH );
+    // write the msg to the file
+    //	  std::ofstream fp;
+    //  fp.open("/tmp/test.png");
+    //  if (!fp) 
+    //	    cgi_fatal("Failed to image file for appending");
+    process_image(
+                  //fp, 
+                  _CONTENT_LENGTH, 
+                  std::string(
+                              std::getenv("CONTENT_TYPE")
+                              )
+                  );
+  } else { 
+  cgi_include("stalinize_results.html"); 
+ }	
+  cgi_include("stalinize_end.html");
 
-	if(getenv("QUERY_STRING")!=NULL)
-	  printf("DEBUG:%s\n",getenv("QUERY_STRING"));
-
-	if(getenv("CONTENT_LENGTH")!=NULL)	  
-	  printf( getenv("CONTENT_LENGTH") );
-
-	if( getenv("HTTP_ACCEPT_LANGUAGE") )
-	  printf( getenv("HTTP_ACCEPT_LANGUAGE") );
-
-	if(getenv("CONTENT_LENGTH")!=NULL){
-	  int _CONTENT_LENGTH = atoi( getenv("CONTENT_LENGTH"));
-
-	  // write the msg to the file
-	  std::ofstream fp;
-	  fp.open("/tmp/test.png");
-	  if (!fp) 
-	    cgi_fatal("Failed to image file for appending");
-	  process_image(fp, 
-			_CONTENT_LENGTH, 
-			std::string(
-				    std::getenv("CONTENT_TYPE")
-				    )
-			);
-	  fp.close();
-	} else {
-	  cgi_include("stalinize_results.html");
-	}	
-	cgi_include("stalinize_end.html");
-	cgi_end();
-	return 0;
+  return 0;
 }
